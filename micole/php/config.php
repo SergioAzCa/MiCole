487ddf7814fe7d5975048c841a6bee12a312db44
SergioAzCa <serazca@gmail.com>
2018-03-15 22:03:14 +0100

    cambios conexion

--- micole/php/config.php
+++ micole/php/config.php
@@ -1,10 +1,10 @@
 <?php
 	/*header('Content-Type: text/html; charset=utf-8');*/
 	$usuario = 'postgres';
-	$contrasenya = 'GmPW#37mzf';
+	$contrasenya = 'micole2018';
 	$bbdd = 'micole';
 	$puerto = '5432';
 	$host = 'localhost';
-	
+
 	$conn = pg_connect("dbname=".$bbdd." user=".$usuario." host=".$host." password=".$contrasenya." port=".$puerto) or die ("Fallo en la conexión");
 ?>
